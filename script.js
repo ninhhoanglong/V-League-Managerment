bc0df6f6c638ab6b6cb637ee6f5230ebe76960da
tranviethungpv <tranviethungpv@gmail.com>
2021-11-12 17:09:34 +0700

    update checkform

--- script.js
+++ script.js
@@ -2,10 +2,17 @@
 
 function closeModal() {
     document.querySelector(".modal1").style.display = "none";
+    document.querySelector(".modal2").style.display = "none";
 }
 
 function openModal() {
     document.querySelector(".modal1").style.display = "flex";
+}
+
+function opendki() {
+    document.querySelector(".modal1").style.display = "none";
+    document.querySelector(".modal2").style.display = "flex";
+
 }
 
 function check(username, password) {
@@ -16,16 +23,54 @@ function check(username, password) {
         alert("Bạn phải điền password!");
         document.myform.password.focus();
     } else {
+        alert("Bạn đã đăng nhập thành công!");
+        var a = document.getElementById("login");
+        a.href="";
+    }
+}
+
+function check1 (username, email, password, password1) {
+    if(username == '') {
+        alert("Bạn phải điền username!");
+        document.myform1.username.focus();
+    } else if(email == '') {
+        alert("Bạn phải điền email!");
+        document.myform1.email.focus();
+    } else if(password == '') {
+        alert("Bạn phải điền password!");
+        document.myform1.password.focus();
+    } else if(password1 == '') {
+        alert("Bạn phải điền lai password!");
+        document.myform1.password1.focus();
+    } else if(username.length <= 10) {
+        alert("Username phải lớn hơn 10 ký tự!");
+        document.myform1.username.focus();
+    } else if(password != password1) {
+        alert("Mật khẩu không trùng nhau!");
+        document.myform1.password1.focus();
+    } else {
         alert("Bạn đã đăng ký thành công!");
+        var a = document.getElementById("dki");
+        a.href="";
     }
 }
 
 function displayResult() {
     var username = document.myform.username.value;
-    // var email = document.myform.email.value;
     var password = document.myform.password.value;
-    // var password1 = document.myform.password1.value;
     check(username, password);
+}
+
+function displayResult1() {
+    var username = document.myform1.username.value;
+    document.myform1.username.focus();
+    var email = document.myform1.email.value;
+    document.myform1.email.focus();
+    var password = document.myform1.password.value;
+    document.myform1.password.focus();
+    var password1 = document.myform1.password1.value;
+    document.myform1.password1.focus();
+    check1(username, email, password, password1);
 }
 
 // end JS-login- Long
