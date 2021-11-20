840f14b8bbc9c6cab51f604004672315d7db3504
Long <ninhhoanglong20@gmail.com>
2021-11-20 12:07:42 +0700

    Long update last time

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // start JS-login- Long
 
+
+
 function closeModal() {
     document.querySelector(".modal1").style.display = "none";
     document.querySelector(".modal2").style.display = "none";
@@ -29,26 +31,64 @@ function check(username, password) {
     }
 }
 
-function check1 (username, email, password, password1) {
+function check1 (username, email, numberphone, password, password1) {
     if(username == '') {
         alert("Bạn phải điền username!");
         document.myform1.username.focus();
-    } else if(email == '') {
+    } 
+    else if(username.length < 7 || username.length > 10) {
+        alert("Username phải từ 7 đến 10 ký tự!");
+        document.myform1.username.focus();
+    } 
+    else if(!checkUsername(username)){
+        alert("Username không có ký tự đặc biệt!");
+        document.myform1.username.focus();
+    }
+    else if(email == '') {
         alert("Bạn phải điền email!");
         document.myform1.email.focus();
-    } else if(password == '') {
+    } 
+    else if(!checkEmail(email)) {
+        alert("Yêu cầu nhập đúng định dạng email!");
+        document.myform1.email.focus();
+    }
+    else if(numberphone == '') {
+        alert("Bạn phải điền number phone!");
+        document.myform1.numberphone.focus();
+    } 
+    else if(numberphone.length != 10) {
+        alert("Phonenumber phải có 10 chữ số!");
+        document.myform1.numberphone.focus();
+    } 
+    else if(!checkNumberphone(numberphone)) {
+        alert("Phonenumber phải là số!");
+        document.myform1.numberphone.focus();
+    }
+    else if(password == '') {
         alert("Bạn phải điền password!");
         document.myform1.password.focus();
-    } else if(password1 == '') {
-        alert("Bạn phải điền lai password!");
+    } 
+    else if(password.length < 7 || password.length > 10) {
+        alert("Password phải từ 7 đến 10 ký tự!");
+        document.myform1.password.focus();
+    } 
+    else if(!checkPassword(password)) {
+        alert("Mật khẩu không hợp lệ!");
+        document.myform1.password.focus();
+    }
+    else if(password == username) {
+        alert("Mật khẩu và Username không được trùng nhau!");
+        document.myform1.password.focus();
+    }
+    else if(password1 == '') {
+        alert("Bạn phải điền lại password!");
         document.myform1.password1.focus();
-    } else if(username.length <= 10) {
-        alert("Username phải lớn hơn 10 ký tự!");
-        document.myform1.username.focus();
-    } else if(password != password1) {
+    } 
+    else if(password != password1) {
         alert("Mật khẩu không trùng nhau!");
         document.myform1.password1.focus();
-    } else {
+    } 
+    else {
         alert("Bạn đã đăng ký thành công!");
         var a = document.getElementById("dki");
         a.href="";
@@ -66,11 +106,33 @@ function displayResult1() {
     document.myform1.username.focus();
     var email = document.myform1.email.value;
     document.myform1.email.focus();
+    var numberphone = document.myform1.numberphone.value;
+    document.myform1.numberphone.focus();
     var password = document.myform1.password.value;
     document.myform1.password.focus();
     var password1 = document.myform1.password1.value;
     document.myform1.password1.focus();
-    check1(username, email, password, password1);
+    check1(username, email, numberphone,password, password1);
+}
+
+function checkUsername(username) {
+    var x = /([a-zA-Z0-9_]{7,10})+$/;
+    return x.test(username);
+}
+
+function checkEmail(email) {
+    var x = /[a-zA-Z0-9_]+@[a-zA-Z0-9_]+(.[a-zA-Z]{2,})+$/;
+    return x.test(email);
+}
+
+function checkNumberphone(numberphone) {
+    var x = /([0-9]{10,10})+$/;
+    return x.test(numberphone);
+}
+
+function checkPassword(password) {
+    var x = /([a-zA-Z0-9]{7,10})+$/;
+    return x.test(password);
 }
 
 // end JS-login- Long
